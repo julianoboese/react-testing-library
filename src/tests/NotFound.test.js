e1507034712d5ffe1d6503b70765d8342c20e5fa
Juliano Boese <juliano.boese@gmail.com>
2022-02-18 18:22:27 -0300

    Requisito 4 - Testa se página 'Not Found' é renderizada corretamente

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../components';
+
+test('A página de "Not Found" deve ser renderizada corretamente', () => {
+  renderWithRouter(<NotFound />);
+
+  const notFoundImage = screen
+    .getByAltText('Pikachu crying because the page requested was not found');
+
+  expect(screen.getByRole('heading', {
+    level: 2, name: 'Page requested not found Crying emoji',
+  }));
+  expect(notFoundImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
