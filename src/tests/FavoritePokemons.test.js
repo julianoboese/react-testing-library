eb67b071c4cdd532d1c1a746d660ea8a5f71de26
Juliano Boese <juliano.boese@gmail.com>
2022-02-18 18:09:52 -0300

    Requisito 3 - Testa se página 'Favorite Pokémons' é renderizada corretamente

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+test('Deve ser exibido "No favorite pokemon found" se não houver pokemons favoritos',
+  () => {
+    renderWithRouter(<FavoritePokemons />);
+    expect(screen.getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+
+test('Devem ser exibidos os pokémons favoritos', () => {
+  const { history } = renderWithRouter(<App />);
+  const AMOUNT_OF_POKEMONS = 9;
+  const favoritePokemons = [];
+
+  for (let i = 0; i < AMOUNT_OF_POKEMONS; i += 2) {
+    for (let j = 0; j < i; j += 1) {
+      userEvent.click(screen.getByRole('button', { name: 'Próximo pokémon' }));
+    }
+    favoritePokemons.push(screen.getByTestId('pokemon-name').innerHTML);
+    userEvent.click(screen.getByRole('link', { name: 'More details' }));
+    userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
+    history.push('/');
+  }
+  history.push('/favorites');
+
+  expect(screen.getAllByTestId('pokemon-name')).toHaveLength(favoritePokemons.length);
+  favoritePokemons.forEach((pokemon) => {
+    expect(screen.getByAltText(`${pokemon} is marked as favorite`)).toBeInTheDocument();
+  });
+});
