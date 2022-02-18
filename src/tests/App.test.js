43a1e8cb7043cd676afa886691a50d907806a796
Juliano Boese <juliano.boese@gmail.com>
2022-02-18 15:21:50 -0300

    Requisito 1 - Testa se localiza os links de navegação

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Os links de navegação são exibidos corretamente', () => {
+  const navLinks = ['Home', 'About', 'Favorite Pokémons'];
+  renderWithRouter(<App />);
+  navLinks.forEach((navLink) => {
+    expect(screen.getByRole('link', { name: navLink })).toBeInTheDocument();
+  });
+});
