c32dc258db58f4a4a52062c54cd7f31e6921a003
Juliano Boese <juliano.boese@gmail.com>
2022-02-19 17:34:26 -0300

    Requisito 6 - Testa se o componente Pokemon funciona corretamente

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,50 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import { Pokemon } from '../components';
+import pokemons from '../data';
+
+test('O card do Pokémon deve exibir os dados corretos', () => {
+  renderWithRouter(<Pokemon
+    pokemon={ pokemons[0] }
+    isFavorite={ false }
+  />);
+
+  const { name, type, averageWeight, image } = pokemons[0];
+  const { value, measurementUnit } = averageWeight;
+
+  expect(screen.getByTestId('pokemon-name').innerHTML).toBe(name);
+  expect(screen.getByTestId('pokemon-type').innerHTML).toBe(type);
+  expect(screen.getByTestId('pokemon-weight').innerHTML).toBe(
+    `Average weight: ${value} ${measurementUnit}`,
+  );
+  expect(screen.getByAltText(`${name} sprite`)).toHaveAttribute('src', image);
+  expect(screen.queryByAltText(`${name} is marked as favorite`)).not.toBeInTheDocument();
+});
+
+test('Caso seja um pokemon favorito, deve exibir o ícone correspondente', () => {
+  renderWithRouter(<Pokemon
+    pokemon={ pokemons[0] }
+    isFavorite
+  />);
+
+  const { name } = pokemons[0];
+
+  expect(screen.getByAltText(`${name} is marked as favorite`))
+    .toHaveAttribute('src', '/star-icon.svg');
+});
+
+test('A tela de detalhes deve ser exibida corretamente ao clicar no link', () => {
+  const { history } = renderWithRouter(<Pokemon
+    pokemon={ pokemons[0] }
+    isFavorite
+  />);
+
+  const pokemonUrl = `/pokemons/${pokemons[0].id}`;
+  const detailsLink = screen.getByRole('link');
+  expect(detailsLink).toHaveAttribute('href', pokemonUrl);
+
+  userEvent.click(detailsLink);
+  expect(history.location.pathname).toBe(pokemonUrl);
+});
