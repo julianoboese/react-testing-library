2d33a83ab36dc7be95ab888471c88692b535ab97
Juliano Boese <juliano.boese@gmail.com>
2022-02-20 19:14:35 -0300

    Organiza código de teste do componente FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -6,29 +6,44 @@ import renderWithRouter from '../renderWithRouter';
 import { FavoritePokemons } from '../components';
 import pokemons from '../data';
 
-test('Deve ser exibido "No favorite pokemon found" se não houver pokemons favoritos',
-  () => {
-    renderWithRouter(<FavoritePokemons />);
-    expect(screen.getByText('No favorite pokemon found')).toBeInTheDocument();
-  });
+describe('03 - Testa o componente <FavoritePokemons.js />', () => {
+  test('Testa se é exibido "No favorite pokemon found" se não houver Pokémons favoritos',
+    () => {
+      renderWithRouter(<FavoritePokemons />);
 
-test('Devem ser exibidos os pokémons favoritos', () => {
-  const { history } = renderWithRouter(<App />);
-  const favoritePokemons = [];
+      const noFavoritesText = screen.getByText('No favorite pokemon found');
+      expect(noFavoritesText).toBeInTheDocument();
+    });
 
-  for (let i = 0; i < pokemons.length; i += 2) {
-    for (let j = 0; j < i; j += 1) {
-      userEvent.click(screen.getByRole('button', { name: 'Próximo pokémon' }));
+  test('Testa se são exibidos todos os Pokémons favoritos', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoritePokemons = [];
+
+    for (let i = 0; i < pokemons.length; i += 2) {
+      for (let j = 0; j < i; j += 1) {
+        const nextPokemon = screen.getByRole('button', { name: 'Próximo pokémon' });
+        userEvent.click(nextPokemon);
+      }
+      const pokemonNameElement = screen.getByTestId('pokemon-name');
+      favoritePokemons.push(pokemonNameElement.innerHTML);
+
+      const detailsLink = screen.getByRole('link', { name: 'More details' });
+      userEvent.click(detailsLink);
+
+      const favoriteCheck = screen.getByLabelText('Pokémon favoritado?');
+      userEvent.click(favoriteCheck);
+
+      history.push('/');
     }
-    favoritePokemons.push(screen.getByTestId('pokemon-name').innerHTML);
-    userEvent.click(screen.getByRole('link', { name: 'More details' }));
-    userEvent.click(screen.getByLabelText('Pokémon favoritado?'));
-    history.push('/');
-  }
-  history.push('/favorites');
 
-  expect(screen.getAllByTestId('pokemon-name')).toHaveLength(favoritePokemons.length);
-  favoritePokemons.forEach((pokemon) => {
-    expect(screen.getByAltText(`${pokemon} is marked as favorite`)).toBeInTheDocument();
+    history.push('/favorites');
+
+    const allFavorites = screen.getAllByTestId('pokemon-name');
+    expect(allFavorites).toHaveLength(favoritePokemons.length);
+
+    favoritePokemons.forEach((pokemon) => {
+      const favoriteIcon = screen.getByAltText(`${pokemon} is marked as favorite`);
+      expect(favoriteIcon).toBeInTheDocument();
+    });
   });
 });
