9d6fcafb8d318fac156809164a533e6ea6142adf
Juliano Boese <juliano.boese@gmail.com>
2022-02-20 17:39:02 -0300

    Organiza código de teste do componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -3,14 +3,17 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import { NotFound } from '../components';
 
-test('A página de "Not Found" deve ser renderizada corretamente', () => {
-  renderWithRouter(<NotFound />);
+describe('04 - Testa o componente <NotFound.js />', () => {
+  test('Testa se a página de "Not Found" é renderizada corretamente', () => {
+    renderWithRouter(<NotFound />);
 
-  const notFoundImage = screen
-    .getByAltText('Pikachu crying because the page requested was not found');
+    const notFoundHeading = screen.getByRole('heading', {
+      level: 2, name: 'Page requested not found Crying emoji',
+    });
+    expect(notFoundHeading).toBeInTheDocument();
 
-  expect(screen.getByRole('heading', {
-    level: 2, name: 'Page requested not found Crying emoji',
-  }));
-  expect(notFoundImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    const notFoundImage = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(notFoundImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
 });
