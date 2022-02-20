c4bcc87909b5dae6599acdb55cac2c0f98f8a4b8
Juliano Boese <juliano.boese@gmail.com>
2022-02-20 19:23:21 -0300

    Organiza código de teste do componente About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,14 +3,21 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import { About } from '../components';
 
-test('A página deve conter os dados sobre a Pokédex', () => {
-  renderWithRouter(<About />);
-  const firstParagraphText = /This application simulates a Pokédex/i;
-  const secondParagraphText = /One can filter Pokémons by type/i;
-  const pokedexImage = screen.getByAltText('Pokédex');
+describe('02 - Testa o componente <About.js />', () => {
+  test('Testa se a página contém os dados sobre a Pokédex', () => {
+    renderWithRouter(<About />);
 
-  expect(screen.getByRole('heading', { level: 2, name: 'About Pokédex' }));
-  expect(screen.getByText(firstParagraphText)).toBeInTheDocument();
-  expect(screen.getByText(secondParagraphText)).toBeInTheDocument();
-  expect(pokedexImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    const pokedexHeading = screen
+      .getByRole('heading', { level: 2, name: 'About Pokédex' });
+    expect(pokedexHeading).toBeInTheDocument();
+
+    const firstParagraphText = screen.getByText(/This application simulates a Pokédex/i);
+    expect(firstParagraphText).toBeInTheDocument();
+
+    const secondParagraphText = screen.getByText(/One can filter Pokémons by type/i);
+    expect(secondParagraphText).toBeInTheDocument();
+
+    const pokedexImage = screen.getByAltText('Pokédex');
+    expect(pokedexImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
